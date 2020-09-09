438fff144938ac330e10bd0e958ff7a59d0bb236
Andrey Zhelezovskiy <azhele@gmail.com>
2020-09-09 13:53:46 +0300

    v1

--- homework.py
+++ homework.py
@@ -2,34 +2,50 @@ import os
 import requests
 import telegram
 import time
+import logging
 from dotenv import load_dotenv
 
 load_dotenv()
 
+logging.basicConfig(
+    filename='running.log',
+    level=logging.DEBUG,
+    format='%(asctime)s %(message)s'
+    )
 
+
+PRACTICUM_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/?'
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+    logging.info(f'homework_name = "{homework_name}", verdict = "{verdict}"')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    try:
+        homework_statuses = requests.get(
+            PRACTICUM_URL,
+            params={'from_date': current_timestamp},
+            headers=headers
+            )
+    except:
+        logging.error('Homework status check error')
     return homework_statuses.json()
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
@@ -45,6 +61,7 @@ def main():
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
+            logging.error(f'Bot returned error: {e}')
             time.sleep(5)
             continue
 
